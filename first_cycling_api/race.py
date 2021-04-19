0792e655d91c61e84d22c1c021724da564d716b0
baronet2 <eth.baron@mail.utoronto.ca>
2021-04-19 14:26:56 -0400

    Added race editions API

--- first_cycling_api/race.py
+++ first_cycling_api/race.py
@@ -1,14 +1,13 @@
 from .utilities import *
-from .race_edition import RaceEdition
 
-# Endpoints ----
+# Race Endpoints ----
 class RaceEndpoint(Endpoint):
 	base_url = 'https://firstcycling.com/race.php'
 
 	def _get_header_details(self):
 		soup = bs4.BeautifulSoup(self.response, 'html.parser')
 		header_details = {}
-		header_details['name'] = soup.h1.text
+		header_details['name'] = soup.h1.text.rsplit(' - ', maxsplit=1)[0] # Get race name, excluding year
 		header_details['links'] = {a.img['src'].rsplit('/', maxsplit=1)[1][:-7]: a['href'] for a in soup.h1.parent.find_all('a')}
 		if 'www' in header_details['links']:
 			header_details['links']['website'] = header_details['links'].pop('www')
@@ -50,6 +49,44 @@ class RaceYoungestOldestWinners(RaceEndpoint):
 		params = {'r': race_id, 'k': 'Y'}
 		self.make_request(params)
 
+# Race Edition Endpoints ----
+class RaceEditionInformation(RaceEndpoint):
+	def __init__(self, race_id, year):
+		params = {'r': race_id, 'y': year, 'k': 2}
+		self.make_request(params)
+
+class RaceEditionResults(RaceEndpoint):
+	def __init__(self, race_id, year, classification_num=None, stage_num=None):
+		params = {'r': race_id, 'y': year, 'l': classification_num, 'e': stage_num}
+		self.make_request(params)
+		self.soup = bs4.BeautifulSoup(self.response, 'html.parser')
+		self._get_results_table()
+		self._get_sidebar_information()
+		del self.soup
+
+	def _get_results_table(self):
+		results_table = self.soup.find('table', {'class': 'sortTabell tablesorter'})
+		self.results_table = table_parser(results_table)
+
+	def _get_sidebar_information(self):
+		return
+
+class RaceEditionStageProfiles(RaceEndpoint):
+	def __init__(self, race_id, year):
+		params = {'r': race_id, 'y': year, 'e': 'all'}
+		self.make_request(params)
+
+class RaceEditionStartlistNormal(RaceEndpoint):
+	def __init__(self, race_id, year):
+		params = {'r': race_id, 'y': year, 'k': 8}
+		self.make_request(params)
+
+class RaceEditionStartlistExtended(RaceEndpoint):
+	def __init__(self, race_id, year):
+		params = {'r': race_id, 'y': year, 'k': 9}
+		self.make_request(params)
+
+
 # Race ----
 class Race(FirstCyclingObject):
 	def __init__(self, race_id):
@@ -80,12 +117,27 @@ class Race(FirstCyclingObject):
 	def get_youngest_oldest_winners(self):
 		return self._get_endpoint(RaceYoungestOldestWinners, 'youngest_oldest_winners')
 
-	def add_edition(self, year, edition):
-		self.editions[year] = edition
-
-	def get_edition(self, year):
+	def _get_edition_endpoint(self, endpoint, year, destination_key, *args, **kwargs):
+		""" Generalized method to load endpoints for race edition """
+		endpoint = endpoint(self.ID, year, *args, **kwargs)
+		self._get_general_info(endpoint)
 		if year in self.editions:
-			return self.editions[year]
+			self.editions[year][destination_key] = endpoint
 		else:
-			self.editions[year] = RaceEdition(self.ID, year)
-			return self.editions[year]
+			self.editions[year] = {destination_key: endpoint}
+		return endpoint
+
+	def get_edition_information(self, year):
+		return self._get_edition_endpoint(RaceEditionInformation, year, 'information')
+
+	def get_edition_results(self, year, classification_num=None, stage_num=None): # Also upddate information from right sidebar if missing
+		return self._get_edition_endpoint(RaceEditionResults, year, 'results', classification_num=classification_num, stage_num=stage_num)
+
+	def get_edition_stage_profiles(self, year):
+		self._get_edition_endpoint(RaceEditionStageProfiles, year, 'stage_profiles')
+
+	def get_edition_startlist_normal(self, year):
+		self._get_edition_endpoint(RaceEditionStartlistNormal, year, 'startlist_normal')
+
+	def get_edition_startlist_extended(self, year):
+		self._get_edition_endpoint(RaceEditionStartlistExtended, year, 'startlist_extended')
