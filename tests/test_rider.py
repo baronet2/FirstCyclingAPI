3c2fd71d32a90023aae49b5ac4d595ba79b986a7
baronet2 <ethanbaron1105@gmail.com>
2023-05-14 23:24:09 -0400

    Update rider test

--- tests/test_rider.py
+++ tests/test_rider.py
@@ -8,6 +8,4 @@ my_vcr = vcr.VCR(cassette_library_dir='tests/vcr_cassettes/rider', path_transfor
 def test_roglic_2020_results():
 	roglic = Rider(18655)
 	results_2020 = roglic.year_results(2020)
-	assert results_2020.sidebar_details['nation'] == 'Slovenia'
-	assert results_2020.sidebar_details['height'] == 1.77
 	assert results_2020.results_df['UCI'].max() == 850
